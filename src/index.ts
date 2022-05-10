93c4c87084741f96457e733a7db56428687a4995
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-10 14:47:41 -0300

    Error if else resuelto Quite comillas al asignar valor a la variable string

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,31 @@
 let mes = document.getElementById("dato3");
 let btnEnviar = document.getElementById("enviar");
 
-//let rotulo1 = document.getElementById("rotulo1");
-//let rotulo2 = document.getElementById("rotulo2");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
 let rotulo3 = document.getElementById("rotulo3");
 
-//rotulo1.innerHTML = "Precio unitario";
-//rotulo2.innerHTML = "Cantidad";
+rotulo1.innerHTML = "Precio unitario";
+rotulo2.innerHTML = "Cantidad";
 rotulo3.innerHTML = "Mes";
 
-//let precioUnitario : number = Number(dato1.value);
-//let cantidad : number = Number(dato2.value);
-let octubre : string = dato3.value;
+
+
 //let precioTotal:number= Number(precioUnitario + cantidad);
 
 
 btnEnviar.addEventListener("click", () => {
-  
- octubre = "octubre"; 
-  
- if ((octubre == "octubre") {
-    console.log("Usted ha obtenido un descuento del 15%");
+  let precioUnitario : number = Number(dato1.value);
+  let cantidad : number = Number(dato2.value);
+  let octubre : string = dato3.value; 
+  let total: number = precioUnitario*cantidad;
+
+octubre = octubre;
+ 
+ if (octubre == "octubre") {
+   let precioConDescuento: number = total - total*0.15;
+    console.log("Usted ha obtenido un descuento del 15%, su total es $ " + precioConDescuento);
   } else {
-    (console.log("Usted no ha obtenido el descuento");
+    (console.log("Usted no ha obtenido el descuento, su total es $ " + total);
   }
 });
