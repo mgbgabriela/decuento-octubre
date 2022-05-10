b213b7b05e254b4c0cd8927e62daf80d5ba8d570
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-09 23:00:58 -0300

    Solo mes Ejercicio solo con variable mes

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
-let precioUnitario = document.getElementById("dato1");
-let cantidad = document.getElementById("dato2");
+//let precioUnitario = document.getElementById("dato1");
+//let cantidad = document.getElementById("dato2");
 let mes = document.getElementById("dato3");
-
 let btnEnviar = document.getElementById("enviar");
-let rotulo1 = document.getElementById("rotulo1");
-let rotulo2 = document.getElementById("rotulo2");
+
+//let rotulo1 = document.getElementById("rotulo1");
+//let rotulo2 = document.getElementById("rotulo2");
 let rotulo3 = document.getElementById("rotulo3");
 
-rotulo1.innerHTML = "Precio unitario";
-rotulo2.innerHTML = "Cantidad";
+//rotulo1.innerHTML = "Precio unitario";
+//rotulo2.innerHTML = "Cantidad";
 rotulo3.innerHTML = "Mes";
 
-let precioUnitario: number = Number(dato1.value);
-let cantidad: number = Number(dato2.value);
-let mes : number = (dato3.value);
-let totalConDescuento:number= 0;
-let descuento: number= 0;
-let precioTotal:number= precioUnitario+cantidad;
+//let precioUnitario : number = Number(dato1.value);
+//let cantidad : number = Number(dato2.value);
+let octubre : string = dato3.value;
+//let precioTotal:number= Number(precioUnitario + cantidad);
+
 
 btnEnviar.addEventListener("click", () => {
-  if (mes =="octubre"); {
-    console.log("Usted ha obtenido un descuento del 20%")
- } else {
-    console.log("Usted no ha obtenido el descuento");
+  
+ octubre = "octubre"; 
+  
+ if ((octubre == "octubre") {
+    console.log("Usted ha obtenido un descuento del 15%");
+  } else {
+    (console.log("Usted no ha obtenido el descuento");
   }
 });
